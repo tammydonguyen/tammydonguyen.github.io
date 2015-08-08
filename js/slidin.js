f658ea786664dac1b923c1890b4a1621d05e58cc
Tammy Nguyen <tammynguyen@berkeley.edu>
2015-08-08 04:16:21 -0700

    cs61a resources page

--- js/slidin.js
+++ js/slidin.js
@@ -1,3 +1,6 @@
+
+
+// index
 $(document).ready(function(){
 	$(".about").click(function(){
 		$("#about").slideToggle("slow");
@@ -17,5 +20,69 @@ $(document).ready(function(){
         $("#work").slideToggle("slow");
         $("#about").slideUp();
         $("#connect").slideUp();
+    });
+});
+
+// solutions
+$(document).ready(function(){
+    $(".solution-toggle").click(function(){
+        $(".solution").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".solution-toggle-2").click(function(){
+        $(".solution-2").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".solution-toggle-3").click(function(){
+        $(".solution-3").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".solution-toggle-4").click(function(){
+        $(".solution-4").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".solution-toggle-5").click(function(){
+        $(".solution-5").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".solution-toggle-6").click(function(){
+        $(".solution-6").slideToggle("slow");
+    });
+});
+
+// solutions with prompt
+$(document).ready(function(){
+    $(".solution-toggle-p").click(function(){
+        $(".solution-p").slideToggle("slow");
+        $(".solution-prompt").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".solution-toggle-2-p").click(function(){
+        $(".solution-2-p").slideToggle("slow");
+        $(".solution-2-prompt").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".solution-toggle-3-p").click(function(){
+        $(".solution-3-p").slideToggle("slow");
+        $(".solution-3-prompt").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".solution-toggle-4-p").click(function(){
+        $(".solution-4-p").slideToggle("slow");
+        $(".solution-4-prompt").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".solution-toggle-5-p").click(function(){
+        $(".solution-5-p").slideToggle("slow");
+        $(".solution-5-prompt").slideToggle("slow");
     });
 });
